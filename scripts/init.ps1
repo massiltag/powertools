423252a674362341456bea53e614c706b3b2118e
Massil TAGUEMOUT <mtaguemout@externe.generali.fr>
2024-09-26 01:41:53 +0200

    feat: add script init

---
+++ scripts/init.ps1
@@ -0,0 +1,6 @@
+# DESCRIPTION : Script rapide à lancer au démarrage du PC pour un setup rapide de l'environnement de travail
+# TAGS : util
+
+px run clean
+px run allowonenote
+
--- scripts/simpleserver.ps1
+++ scripts/simpleserver.ps1
@@ -1,4 +1,4 @@
-# DESCRIPTION : CrÃ©e un serveur simple sur localhost:9999 avec un GET /hello qui retourne "HELLO WORLD!"
+# DESCRIPTION : Crée un serveur simple sur localhost:9999 avec un GET /hello qui retourne "HELLO WORLD!"
 # TAGS : util, dev
 
 # Create and configure the HttpListener
@@ -13,13 +13,26 @@ while ($true) {
     $request = $context.Request
     $response = $context.Response
 
+    # Add CORS headers to allow all origins
+    $response.Headers.Add("Access-Control-Allow-Origin", "*")
+    $response.Headers.Add("Access-Control-Allow-Headers", "*") # Allow all headers
+    $response.Headers.Add("Access-Control-Allow-Methods", "GET, OPTIONS") # Allow GET and OPTIONS methods
+
     # Handle the request
+    if ($request.HttpMethod -eq "OPTIONS") {
+        # Handle preflight request
+        $response.StatusCode = 204 # No Content
+        $response.OutputStream.Close()
+        continue
+    }
+
     if ($request.HttpMethod -eq "GET" -and $request.Url.AbsolutePath -eq "/hello") {
-        # Log the call to /hello in green
-        Write-Host "Received GET request for /hello" -ForegroundColor Green
+        # Log the current time and the call to /hello in green
+        $currentTime = Get-Date -Format "yyyy-MM-dd HH:mm:ss"
+        Write-Host "[$currentTime] Received GET request for /hello" -ForegroundColor Green
 
         # Prepare the response
-        $responseString = "HELLO WORDL!"
+        $responseString = "HELLO WORLD!"
         $buffer = [System.Text.Encoding]::UTF8.GetBytes($responseString)
         $response.ContentLength64 = $buffer.Length
         $response.OutputStream.Write($buffer, 0, $buffer.Length)
@@ -34,4 +47,3 @@ while ($true) {
 }
 
 # To stop the server, use Ctrl+C in the terminal
-
